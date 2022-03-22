2d42c0e695174e20d792c7dfb97e3b6f6ac749e1
Mohamed Boumlik <mohamedboumlik000@gmail.com>
2022-03-22 21:08:24 +0100

    first commit

--- webpack.config.js
+++ webpack.config.js
@@ -112,6 +112,10 @@ module.exports={
 			filename:"search.html",
 			template:"./src/search.html",
 		}),
+		new HtmlWebpackPlugin({
+			filename:"contact.html",
+			template:"./src/contact.html",
+		}),
 		
 
 		new MiniCssExtractPlugin({filename: 'css/style.css'}),
